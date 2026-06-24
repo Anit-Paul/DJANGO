36b67894a54fd81ec4c48acf9e0e93efecb45829
Anit Paul <anit42655@gmail.com>
2026-06-24 16:02:34 +0000

    about frontend modified

--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.homeAPI, name='home'),
     path('about/', include('about.urls')),
+    
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
