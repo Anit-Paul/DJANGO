3737cb2a6e80e02e52f1b659d25b57301f35f7ed
Anit Paul <anit42655@gmail.com>
2026-06-24 14:30:43 +0000

    views modified for home url

--- mydjango/mydjango/views.py
+++ mydjango/mydjango/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def homeAPI(request):
-    return HttpResponse("Hello, this is the home API view.")
+    #return HttpResponse("Hello, this is the home API view.")
+    return render(request, 'home/index.html')
