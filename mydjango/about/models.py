04d7420a61df8ba14cb5c3368800fa4009779ce6
Anit Paul <anit42655@gmail.com>
2026-06-25 14:38:39 +0000

    model created

--- mydjango/about/models.py
+++ mydjango/about/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Student(models.Model):
+    MY_SCHOOL=[
+        ('AB','AB School'),
+        ('BB','BB School'),
+        ('CB','CB School'),
+    ]
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='photos/')
+    date=models.DateTimeField(default=timezone.now)
+    school=models.CharField(max_length=2,choices=MY_SCHOOL)
